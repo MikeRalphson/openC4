c55a9df1fb59c07486aa902edaa9813734ff8c0e
Mike Ralphson <mike.ralphson@gmail.com>
2016-11-28 15:06:26 +0000

    Fix up examples

--- example1.js
+++ example1.js
@@ -3,12 +3,14 @@ var nitro = require('bbcparse/nitroSdk.js');
 var xml2j = require('jgexml/xml2json.js');
 
 var query = nitro.newQuery();
-query.add(c4.commonPlatformC4,'',true);
+query.add(c4.commonPlatformC4,'',false);
 
 var options = {};
-options.Accept = 'application/xml';
+options.headers = {
+	Accept: 'application/xml'
+};
 options.api_key_name = 'apikey';
-nitro.make_request(c4.host,c4.getAtoz2('A'),process.env.C4_API_KEY,query,options,function(obj){
+nitro.make_request(c4.host,c4.getAtoz2('A'),'',query,options,function(obj){
     var json = xml2j.xml2json(obj);
     console.log(JSON.stringify(json,null,2));
 });
--- example2.js
+++ example2.js
@@ -3,15 +3,17 @@ var nitro = require('bbcparse/nitroSdk.js');
 var xml2j = require('jgexml/xml2json.js');
 
 var query = nitro.newQuery();
-query.add(c4.commonPlatformC4,'',true);
+query.add(c4.commonPlatformC4,'',false);
 
 var options = {};
-options.Accept = 'application/xml';
+options.headers = {
+	Accept: 'application/xml'
+};
 options.api_key_name = 'apikey';
 
 var cat = process.argv.length > 2 ? process.argv[2] : 'comedy';
 
-nitro.make_request(c4.host,c4.getCategories4od(cat),process.env.C4_API_KEY,query,options,function(obj){
+nitro.make_request(c4.host,c4.getCategories4od(cat),'',query,options,function(obj){
     var json = xml2j.xml2json(obj);
     console.log(JSON.stringify(json,null,2));
 });
